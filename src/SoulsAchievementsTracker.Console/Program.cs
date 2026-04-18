31bb7cc483983e69f3386fca157a3da7fced2b23
xornis <xornis.dev@gmail.com>
2026-04-18 03:10:55 +0300

    Tried to find .co2 and .sl2 save files

--- src/SoulsAchievementsTracker.Console/Program.cs
+++ src/SoulsAchievementsTracker.Console/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace SoulsAchievementsTracker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("=== Dark Souls Achievement Tracker ===\n");
+
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string saveFolderPath = Path.Combine(documentsPath, "NBGI", "DARK SOULS REMASTERED");
+
+            Console.WriteLine($"Searching in: {saveFolderPath}\n");
+
+            if (Directory.Exists(saveFolderPath))
+            {
+                Console.WriteLine("Save folder found!");
+
+                var steamIdFolders = Directory.GetDirectories(saveFolderPath);
+                Console.WriteLine($"\nFound {steamIdFolders.Length} Steam ID folder(s):");
+
+                foreach (var folder in steamIdFolders)
+                {
+                    Console.WriteLine();
+                    string steamId = Path.GetFileName(folder);
+                    Console.WriteLine($"Steam ID: {steamId}");
+                    Console.WriteLine();
+
+                    var slSaveFiles = Directory.GetFiles(folder, "*.sl2");
+                    var coSaveFiles = Directory.GetFiles(folder, "*.co2");
+
+                    Console.WriteLine($".sl2 Save files found: {slSaveFiles.Length}");
+
+                    foreach (var slSaveFile in slSaveFiles)
+                    {
+                        FileInfo fileInfo = new FileInfo(slSaveFile);
+                        Console.WriteLine($"- {Path.GetFileName(slSaveFile)} ({fileInfo.Length / 1024} KB)");
+                    }
+
+                    
+                    Console.WriteLine($".co2 Save files found: {coSaveFiles.Length}");
+
+                    foreach (var coSaveFile in coSaveFiles)
+                    {
+                        FileInfo fileInfo = new FileInfo(coSaveFile);
+                        Console.WriteLine($"- {Path.GetFileName(coSaveFile)} ({fileInfo.Length / 1024} KB)");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Save folder NOT found!");
+                Console.WriteLine("Make sure Dark Souls Remastered is installed.");
+            }
+        }
+    }
+}
+
